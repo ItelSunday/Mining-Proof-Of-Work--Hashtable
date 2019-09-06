f4ee8df91e6908c7df0dc11dcb3ec108c4a18c09
ItelSunday <iteldomingotech@gmail.com>
2019-09-06 16:32:07 -0500

    start mining for lambda coin

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,7 +25,9 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    #  TODO: 
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +41,12 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    # TODO: Your code here! ###########
+    hash_guess_proof = f'{proof}'.encode()
+    last_hash = f'{last_hash}'
+    hash_proof =  hashlib.sha256(hash_guess_proof).hexdigest()
+    
+    return hash_proof[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
